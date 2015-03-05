aaa304b9942532db7dc777af0f2b1ce0b7512b9f
jcbigtree <chashan2000@gmail.com>
2015-03-05 22:03:01 +0000

    changing commenting, fixed bug

--- include/BenchmarkFunctions.hpp
+++ include/BenchmarkFunctions.hpp
@@ -8,20 +8,26 @@
 
 namespace EC
 {
+	/// \brief Sphere functor
 	class SphereFunctor : public BaseFitnessFunctor <double, double>
 	{
 	public:
 		SphereFunctor();
 		virtual ~SphereFunctor();
 
-		/// \brief     Calculate fitness of an individual. For single-objective optimization only.
-		/// \param[in] BaseIndividual<ChromoType, FitnessType>&. A reference to an individual
-		virtual double operator() (BaseIndividual<double, double>* pIndividual);
+		/// \brief Calculate fitness of an individual. For single-objective optimization only.
+		/// \param[in] pIndiv. Individual that is to be evaluated
+		virtual double operator() (BaseIndividual<double, double>* pIndiv);
 
+		/// \brief Get the domain lower bound
+		/// \return the domain lower bound
 		inline std::vector<double>& GetDomainLowerBound()
 		{
 			return m_lowerBound;
 		}
+
+		/// \brief Get the domain upper bound
+		/// \return the domain upper bound
 		inline std::vector<double>& GetDomainUpperBound()
 		{
 			return m_upperBound;
@@ -30,7 +36,7 @@ namespace EC
 	protected:		
 		std::vector<double> m_lowerBound;
 		std::vector<double> m_upperBound;
-		int m_problemDim;
+		unsigned int m_problemDim;
 	};
 
 }
